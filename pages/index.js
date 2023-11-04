180db08e8418245cb5bcd9131414112098b32ea6
Pratham Agarwal <109911989+UniquePratham@users.noreply.github.com>
2023-11-04 19:45:35 +0530

    SlideBar

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
 // pages/index.js
-
+import React from "react";
+import { Box, ChakraProvider } from "@chakra-ui/react";
 import Navbar from "../components/Navbar";
+import SlideBar from "../components/SlideBar";
+import Footer from "../components/Footer";
+
+const images = ["img (1)", "img (2)", "img (3)", "img (4)", "img (5)"];
 
 const Home = () => {
   return (
     <div>
       {/* Your other page content goes here */}
       <Navbar />
+      <ChakraProvider>
+        <Box>
+          <SlideBar images={images} />
+        </Box>
+      </ChakraProvider>
       <Footer />
     </div>
   );

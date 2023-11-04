8d7aacc6de33939c57fb547af57ceecbbab14498
subhro1530 <shaswata.ssaha@gmail.com>
2023-11-04 20:02:28 +0530

    made Navbar

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* Font Awesome CDN */}
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          />
+
+          {/* Favicon */}
+          <link rel="icon" href="/logo.png" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
